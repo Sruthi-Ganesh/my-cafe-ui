789f6f8408a98e5c3480ad62cec6eeba826d2de8
Sruthi Ganesh <sruthibadal@gmail.com>
2024-10-02 13:36:58 +0800

    Open confirmation dialog when modal clicked outside

--- src/common/modal/index.tsx
+++ src/common/modal/index.tsx
@@ -43,7 +43,7 @@ export const CModal = (props: ModalProps) => {
     <>
       <Modal
         open={props.open}
-        onClose={handleCloseModals}
+        onClose={() => setOpenConfirmationDialog(true)}
         className="create-modal"
       >
         <Box sx={BoxStyle}>
